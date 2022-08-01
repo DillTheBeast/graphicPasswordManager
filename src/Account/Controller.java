9450073506c4916f591cc53e85b8a52680b4d03c
DillTheBeast <dillonmaltese@gmail.com>
2022-08-01 09:33:55 -0400

    t

--- src/Account/Controller.java
+++ src/Account/Controller.java
@@ -2,9 +2,18 @@ package Account;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Label;
+import javafx.scene.Node;
 
 public class Controller {
+
+    Parent root;
+    Stage stage;
+    Scene scene;
 
     @FXML
     private Label label;
@@ -14,7 +23,17 @@ public class Controller {
     }
     
     @FXML
-    void onEnterClick(ActionEvent event) {
-        
+    void onLoginClick(ActionEvent event) {
+        try {
+            //Transfering to the next Screen
+            root = FXMLLoader.load(this.getClass().getResource("/fxml/home.fxml"));
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            System.out.println("Scene is not able to be switched");
+        }
     }
 }                                                                                                                                                                      
