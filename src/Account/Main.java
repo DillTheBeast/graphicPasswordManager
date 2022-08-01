3feb8560720e0fdf20e7a6ced4e03ff44f3f47c1
DillTheBeast <dillonmaltese@gmail.com>
2022-08-01 09:19:16 -0400

    t

--- src/Account/Main.java
+++ src/Account/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Account.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
