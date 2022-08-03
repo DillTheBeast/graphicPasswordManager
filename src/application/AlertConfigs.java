70303d053e91796f312109e05fb147bd5869cd92
DillTheBeast <dillonmaltese@gmail.com>
2022-08-03 10:15:02 -0400

    t

--- src/application/AlertConfigs.java
+++ src/application/AlertConfigs.java
@@ -9,4 +9,7 @@ public class AlertConfigs {
     public static Alert unknownErrorAlert = new Alert(AlertType.ERROR, "There is an unknown error");
 
     public static Alert invalidCredentialsAlert = new Alert(AlertType.ERROR, "The username and password that were given are invalid");
+
+    //Invalid input manages most of the errors and when you need to use it you just reset the text right before calling it
+    public static Alert invalidInput = new Alert(AlertType.ERROR, "");
 }
--- src/application/SignUpController.java
+++ src/application/SignUpController.java
@@ -1,5 +1,11 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import DBHandler.DBHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -43,12 +49,33 @@ public class SignUpController {
     @FXML
     private TextField usernameField;
 
-    public void initialize() {
+    DBHandler handler;
+    Connection connection;
+    Statement stmt;
+    ResultSet rs;
 
+    public void initialize() {
+        handler = new DBHandler();
     }
 
     @FXML
-    void onEnterClick(ActionEvent event) {
+    void onEnterClick(ActionEvent event) throws SQLException {
+        //See if any fields are empty
+        if(areFieldsEmpty()) {
+            AlertConfigs.invalidInput.setContentText("There is an empty text field that needs to have text in it");
+            AlertConfigs.invalidInput.showAndWait();
+        }
+        else {
+            if(isInvalidUsername()) {
+                AlertConfigs.invalidInput.showAndWait();
+            }
+            else{
+                System.out.println("Hi");
+            }
+        }
+    }
+
+    public boolean areFieldsEmpty() {
         if(usernameField.getText().equals("") 
         || passwordField.getText().equals("") 
         || firstNameField.getText().equals("") 
@@ -60,10 +87,40 @@ public class SignUpController {
         || serviceField.getText().equals("")
         || servicePasswordFIeld.getText().equals("")
         || serviceUsernameField.getText().equals("")) {
-            System.out.println("Hi");
+            return true;
         }
         else {
-            System.out.println("Test");
+            return false;
         }
+    }
+
+    public boolean isInvalidUsername() throws SQLException {
+        if(usernameField.getText().length() < 5) {
+            AlertConfigs.invalidInput.setContentText("The username given is too small. It needs to be five or more letters");
+            return true;
+        }
+
+        boolean isInvalidUsername = true;
+        connection = handler.getConnection();
+
+        String query = "SELECT * FROM password_manager.credentials WHERE username=\'"  + usernameField.getText() + "\'";
+
+        stmt = connection.createStatement();
+        rs = stmt.executeQuery(query);
+
+            if(!rs.next()) {
+                AlertConfigs.invalidInput.setContentText("This username is already taken. Please select a different username.");
+                AlertConfigs.invalidInput.showAndWait();
+                isInvalidUsername = false;
+            }
+            else {
+
+            }
+        
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return isInvalidUsername;
     }
 }
